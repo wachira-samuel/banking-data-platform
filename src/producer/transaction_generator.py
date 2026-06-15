b9e43db138ddffb5a280d966360e1036d48458d6
Samuel Wachira <156680231+wachira-samuel@users.noreply.github.com>
2026-06-15 15:29:39 +0300

    Add transaction generator for synthetic banking data

---
+++ src/producer/transaction_generator.py
@@ -0,0 +1,37 @@
+import uuid
+import random
+from faker import Faker
+from datetime import datetime
+
+faker = Faker()
+
+MERCHANTS = [
+	"Naivas",
+	"Carrefour",
+	"Quickmart",
+	"Jumiapay",
+	"Safaricom",
+	"KCB Bank"
+]
+
+CHANNELS = [
+	"ATM",
+	"POS",
+	"Mobile  Banking",
+	"Internet Banking"
+]
+
+#generate Synthetic banking transaction
+def generate_transaction() -> dict:
+	transaction = {
+	"transaction_id" : str(uuid.uuid4()),
+	"account_id" : random.randint(1000,99999),
+	"amount": round(random.uniform(100, 10000), 2),
+	"channel": random.choice(CHANNELS),
+	"merchant" :random.choice(MERCHANTS),
+	"location": faker.city(),
+	"timestamp" : datetime.utcnow().isoformat()
+	}
+	return transaction
+if __name__ =="__main__":
+	print(generate_transaction())
