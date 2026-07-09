8291f844ec5d3880d699c8c9a389cdfcff13f720
wachira-samuel <ngari.samuel21@students.dkut.ac.ke>
2026-07-09 23:20:51 +0300

    Refractor gitignore files

--- orchestration/dags/banking_pipeline.py
+++ orchestration/dags/banking_pipeline.py
@@ -1,40 +1,17 @@
 from airflow import DAG
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
 from datetime import datetime
-
-from ingestion.producer import main as producer_main
-
-
-default_args = {
-    "owner": "sam"
-}
-
 
 with DAG(
     dag_id="banking_pipeline",
-    default_args=default_args,
-    start_date=datetime(2026, 6, 20),
+    start_date=datetime(2026, 7,09),
     schedule=None,
-    catchup=False
+    catchup=False,
 ) as dag:
 
-    start_producer = PythonOperator(
-        task_id="start_producer",
-        python_callable=producer_main
-    )
+    start = EmptyOperator(task_id="start")
+    end = EmptyOperator(task_id="end")
 
-    start_streaming = SparkSubmitOperator(
-        task_id="start_streaming",
-        application="/opt/project/src/streaming/spark_streaming.py",
-        conn_id="spark_default",
-        name="banking-streaming-job",
-        conf={
-            "spark.master": "spark://spark-master:7077",
-            "spark.app.name": "BankingStreaming",
-            "spark.executor.memory": "1g",
-            "spark.driver.memory": "1g"
-        }
-    )
+    start >> end
 
-    start_producer >> start_streaming
+
